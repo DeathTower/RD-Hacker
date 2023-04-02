df5a30200df82ded0070acf6a711aa89a9986eb5
DeathTower <123762450+DeathTower@users.noreply.github.com>
2023-04-02 14:15:31 +0300

    Add files via upload

---
+++ batRD6.bat
@@ -0,0 +1,47 @@
+@echo off
+title RD Hacker 32bit
+color 02
+echo Welcome to Rd Hacker 32bit!
+echo Scanning has been started if you press any key
+pause
+color 1
+cls
+echo Scan... 24%
+cls
+echo Scan..  56%
+cls
+echo Scan.   96%
+cls
+echo Scaning Complited!
+pause
+cls
+start
+cls
+start
+cls
+start
+cls
+start notepad
+cls
+color 4
+echo Opened: Cmd, Cmd, Cmd, Notepad
+pause
+cls
+start
+start
+start
+start notepad
+start notepad
+start internet explorer
+start
+start internet explorer
+Dir/s
+pause
+cls
+color 02
+set /p input=Enter Command:
+if %input%==help echo scan, set
+if not %input%==help echo reboot, var
+if %input%==scan dir/s
+if not %input%==scan echo unknown command
+pause
