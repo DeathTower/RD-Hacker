74ed5db7680dd3f0a2a33ef7f49972912fbebdda
DeathTower <123762450+DeathTower@users.noreply.github.com>
2023-04-03 10:57:06 +0300

    Add files via upload

---
+++ Load.bat
@@ -0,0 +1,9 @@
+@echo off
+title RD-Hacker C:/users/user/-1.14-windows.xml Loader and injector
+echo =========LOADING START HERE=========
+pause
+cls
+dir/s
+pause
+echo =========LOADING HAS COMPLETE 38% HAS BEEN NOT LOADED========
+pause
